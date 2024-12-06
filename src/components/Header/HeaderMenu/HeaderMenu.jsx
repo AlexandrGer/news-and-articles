393c9b84b9a0068b499968afbd3be34489880318
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-12-06 16:47:25 +0300

    [*] - Обучение и ресурсы теперь раскрываются

--- src/components/Header/HeaderMenu/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu/HeaderMenu.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import useWindoWidth from "../../../utils/useWindowWidth";
 import "./HeaderMenu.scss";
 
 export default function HeaderMenu() {
   const windowWidth = useWindoWidth();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="header-menu">
       <div className="container">
-        <div className="header-menu__container">
+        <nav className="header-menu__container">
           <img src="./images/logo.png" alt="logo" className="logo" />
           {windowWidth >= 424 && (
             <ul className="menu">
@@ -23,9 +28,30 @@ export default function HeaderMenu() {
               </li>
               {windowWidth >= 539 && (
                 <>
-                  <li>
-                    <a href="#">Обучение и ресурсы</a>
-                    <span></span>
+                  <li style={{ position: "relative" }} onClick={toggleMenu}>
+                    <p>Обучение и ресурсы</p>
+                    <button
+                      className={isOpen ? "arrow arrow_up" : "arrow "}
+                    ></button>
+                    {isOpen && (
+                      <ul className="menu__list">
+                        <li>
+                          <a href="#">Что такое ElcorePLC</a>
+                        </li>
+                        <li>
+                          <a href="#">Документация</a>
+                        </li>
+                        <li>
+                          <a href="#">Обучающие материалы</a>
+                        </li>
+                        <li>
+                          <a href="#">Примеры использования</a>
+                        </li>
+                        <li>
+                          <a href="#">База знаний</a>
+                        </li>
+                      </ul>
+                    )}
                   </li>
                   <li>
                     <a href="#">Новости и статьи</a>
@@ -37,7 +63,7 @@ export default function HeaderMenu() {
           <button className="burger-menu">
             <span className="burger-menu__lines"></span>
           </button>
-        </div>
+        </nav>
       </div>
     </div>
   );
