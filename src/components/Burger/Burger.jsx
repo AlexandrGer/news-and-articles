adc9c24ae6b48938890c6ab250c490308132e5e0
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-12-10 09:30:50 +0300

    [*] - Работающий бургер

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Burger.scss";
 
 export default function BurgerMenu() {
+  const [burgerOpen, setBurgerOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleBurger = () => {
+    setBurgerOpen(!burgerOpen);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <button className="burger-menu">
-      <span className="burger-menu__lines"></span>
-    </button>
+    <div style={{ position: "relative" }}>
+      <button className="burger-menu" onClick={toggleBurger}>
+        <span
+          className={` ${
+            burgerOpen
+              ? "burger-menu__lines burger-menu__lines_open"
+              : "burger-menu__lines"
+          }`}
+        ></span>
+      </button>
+      {burgerOpen && (
+        <nav className="burger-menu_nav">
+          <ul>
+            <li>
+              <a href="#">ElcoreStore</a>
+            </li>
+            <li>
+              <a href="#">ElcoreCloud</a>
+            </li>
+            <li>
+              <a href="#">ElcoreIDE</a>
+            </li>
+            <li
+              className={`${
+                menuOpen
+                  ? "burger-menu__list burger-menu__list_open"
+                  : "burger-menu__list"
+              }`}
+              style={{ position: "relative" }}
+              onClick={toggleMenu}
+            >
+              <div>
+                <p>Обучение и ресурсы</p>
+                <button
+                  className={menuOpen ? "arrow arrow_up" : "arrow "}
+                ></button>
+              </div>
+
+              {menuOpen && (
+                <ul>
+                  <li>
+                    <a href="#">Что такое ElcorePLC</a>
+                  </li>
+                  <li>
+                    <a href="#">Документация</a>
+                  </li>
+                  <li>
+                    <a href="#">Обучающие материалы</a>
+                  </li>
+                  <li>
+                    <a href="#">Примеры использования</a>
+                  </li>
+                  <li>
+                    <a href="#">База знаний</a>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <a href="#">Новости и статьи</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 }
